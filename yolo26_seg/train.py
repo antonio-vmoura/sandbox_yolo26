70a3b2a22573336cedccb36ae1c0c904d39101db
Antônio Rodrigues <avmoura.r@gmail.com>
2026-03-12 13:35:55 +0000

    :sparkles: add: new model

--- yolo26_seg/train.py
+++ yolo26_seg/train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 def main():
     # 1. Carrega o modelo pré-treinado de segmentação (versão 'nano' para iniciar)
-    model = YOLO("/workspace/cache/yolo26n-seg.pt")
+    model = YOLO("/workspace/cache/yolo26m-seg.pt")
 
     # 2. Inicia o Fine-Tuning
     results = model.train(
@@ -11,7 +11,7 @@ def main():
         epochs=100, # Número de épocas
         imgsz=640, # Tamanho da imagem
         project="/workspace/logs", # Onde salvar os resultados (mapeado para sua pasta host)
-        name="ph2_finetuning", # Nome da pasta do experimento
+        name="ph2_finetuning_medium", # Nome da pasta do experimento
         device=0, # Usa a GPU 0
         workers=4, # Threads para carregar dados
         batch=16 # Tamanho do lote (ajuste conforme a VRAM da sua GPU)
