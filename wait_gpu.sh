93ce4a2714c48cdb0aba4e4dad90cef619b91f7f
Antônio Rodrigues <avmoura.r@gmail.com>
2026-04-29 16:46:50 +0000

    :sparkles: add: new analysis

--- wait_gpu.sh
+++ wait_gpu.sh
@@ -75,21 +75,21 @@ done
 #   python /workspace/yolo26_seg/train_isic_2018_task_1_v2.py 2>&1 | tee logs/yolo26_small_ft_isic_2018_v2.log
 
 
-# docker run --gpus all -it --rm \
-#   --ipc=host \
-#   --user $(id -u):$(id -g) \
-#   -e TORCH_HOME=/workspace/cache/torch \
-#   -e HOME=/workspace/cache \
-#   -e PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
-#   -v $(pwd)/datasets:/workspace/datasets \
-#   -v $(pwd)/logs:/workspace/logs \
-#   -v $(pwd)/yolo26_seg:/workspace/yolo26_seg \
-#   -v $(pwd)/utils:/workspace/utils \
-#   -v $(pwd)/cache:/workspace/cache \
-#   -v /etc/passwd:/etc/passwd:ro \
-#   -v /etc/group:/etc/group:ro \
-#   yolo26_ft \
-#   python /workspace/yolo26_seg/train_isic_2018_task_1_v7.py --model small 2>&1 | tee logs/yolo26_small_ft_isic_2018_v7_hed.log
+docker run --gpus all -it --rm \
+  --ipc=host \
+  --user $(id -u):$(id -g) \
+  -e TORCH_HOME=/workspace/cache/torch \
+  -e HOME=/workspace/cache \
+  -e PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
+  -v $(pwd)/datasets:/workspace/datasets \
+  -v $(pwd)/logs:/workspace/logs \
+  -v $(pwd)/yolo26_seg:/workspace/yolo26_seg \
+  -v $(pwd)/utils:/workspace/utils \
+  -v $(pwd)/cache:/workspace/cache \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  yolo26_ft \
+  python /workspace/yolo26_seg/train_isic_2018_task_1_v8.py --model nano 2>&1 | tee logs/yolo26_nano_ft_isic_2018_v8.log
 
 
 docker run --gpus all -it --rm \
